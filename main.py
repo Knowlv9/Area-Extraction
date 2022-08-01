131101ef7ded7fbc40ebf2d5c70af793edf05319
Sakaguchi <80980441+Knowlv9@users.noreply.github.com>
2022-08-01 11:50:52 +0900

    annotation on screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, json
 import eel
 import configparser
 import shutil
@@ -10,13 +10,40 @@ conf_default = configparser.ConfigParser()
 conf_default.read("./conf/conf.ini", encoding="utf-8")
 conf = conf_default["DEFAULT"]
 
-dataloader = DataLoader(conf["SOURCE"], conf["DATASETS"])
+info = {}
+with open("./conf/setting.json", 'r', encoding="utf-8") as f:
+	info = json.load(f)
+f.close()
 
+dataloader = DataLoader(conf["SOURCE"], conf["DATASETS"], info)
+
+'''
+	dashboard
+'''
 @eel.expose
-def home():
-	print("home")
+def get_progress(d):
+	result = dataloader.get_progress(str(d))
+	# print(result)
+	return result
+
+'''
+	classify
+'''
+@eel.expose
+def classify():
+	# print("home")
+	# print(info)
+	dataloader = DataLoader(conf["SOURCE"], conf["DATASETS"], info)
 	directories = dataloader.get_dirs()
 	return directories
+
+@eel.expose
+def get_json():
+	info = {}
+	with open("./conf/setting.json", 'r', encoding="utf-8") as f:
+		info = json.load(f)
+	f.close()
+	return info
 
 @eel.expose
 def get_data(path, d):
@@ -26,8 +53,19 @@ def get_data(path, d):
 	return data
 
 @eel.expose
-def update_class(idx, classify):
-	return dataloader.update_class(idx, classify)
+def update_class(idx, data):
+	return dataloader.update_class(idx, data)
+
+@eel.expose
+def add_df(idx, dic):
+	dataloader.add_new(idx, dic)
+	return None
+'''
+	Display
+'''
+@eel.expose
+def display():
+	return dataloader.display()
 
 def onCloseWindow(page, sockets):
 	print("close App")
@@ -38,10 +76,14 @@ def main():
 	eel.init("UI")
 	# eel.echo("hello world!")
 	eel.start(
-		"classify.html",
+		# "classify.html",
+		"dashboard.html",
 		size=(int(conf["GUI_WIDTH"]), int(conf["GUI_HEIGHT"])),
 		# close_callback=onCloseWindow
 	)
 
 if __name__ == "__main__":
 	main()
+	# path = "./LC20_1024/1002173/1002173_10072_33600_1024.png"
+	# d = "1002173"
+	# data = get_data(path, d)
